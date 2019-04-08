c84e64faa6aa862970ae43333362087a6740ea45
Vikasini <vikasininithi@gmail.com>
2019-04-08 16:18:52 +0530

    changes in product filter

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { AppSetting } from '../config/appSetting';
 import {Product} from './view-products/product.model';
+import {Filter} from './view-products/filter.model';
 
 @Injectable({
   providedIn: 'root'
@@ -32,5 +33,12 @@ export class ProductService {
     const filterURL = 'productSettings/';
     const url: string = this.serviceUrl + filterURL;
     return this.httpClient.get<Product>(url);
+  }
+
+// filter method
+  filterByColor(id, data): Observable<any> {
+    const categoryUrl = 'categoryColor/';
+    const url: string = this.serviceUrl + categoryUrl + id;
+    return this.httpClient.put<Product>(url, data);
   }
 }
